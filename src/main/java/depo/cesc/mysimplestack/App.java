e58e339577769ae8fc3ffc9777959c0b6e92b5ed
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2020-10-22 08:29:48 +0200

    #2 Synchro des taches sur IntelliJ

--- src/main/java/depo/cesc/mysimplestack/App.java
+++ src/main/java/depo/cesc/mysimplestack/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "issue #1" );   // issue #1
+        System.out.println( "synchro #2");  // synchronisation #2
     }
 }
